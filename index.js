52d5024a0f913d709b03f4630ebed59bc5f1d94e
Saad Zafar <31594740+saadie123@users.noreply.github.com>
2018-03-22 09:41:38 +0500

    Added mongoose and database connection

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+mongoose.connect('mongodb://saadie:saadie@ds121599.mlab.com:21599/job-portal');
+mongoose.Promise = global.Promise;
 
 app.get('/', (req, res) => {
     res.send({sample: 'Testing route'});
-})
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-})
+});
