1e22e2ba8c60fd5eaa457729edd542f97327bdcc
Saad Zafar <31594740+saadie123@users.noreply.github.com>
2018-03-29 18:55:38 +0500

    Added login, register form

--- client/src/components/User/Login/Login.js
+++ client/src/components/User/Login/Login.js
@@ -1,9 +1,51 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
-const Login = () => {
-    return(
-        <h1>Login Component</h1>
-    )
+import './Login.css';
+class Login extends Component {
+    state = {
+        loginForm:{
+            email: '',
+            password: ''
+        },
+        token: ''
+    }
+    inputChangeHandler = (e, inputName) => {
+        let updatedLoginForm = {
+            ...this.state.loginForm
+        }
+        updatedLoginForm[inputName] = e.target.value;
+        this.setState({loginForm: updatedLoginForm});
+    }
+    onLogin = (e) => {
+        e.preventDefault();
+        axios.post('/user/login',this.state.loginForm).then(response=>{
+            console.log(response);
+        });
+    }
+
+    render(){
+        return(
+            <div className="row">
+            <div className="login-form">
+                <h1 className="page-header">Login</h1>        
+                    <div className="col-md-6">
+                    <form action="">
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" onChange={(event) => this.inputChangeHandler(event,'email')} className="form-control"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" onChange={(event) => this.inputChangeHandler(event,'password')} className="form-control"/>
+                        </div>
+                        <button onClick={(event) => this.onLogin(event)} className="btn btn-primary" type="submit">Login</button>
+                    </form>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Login;
--- client/src/components/User/Register/Register.js
+++ client/src/components/User/Register/Register.js
@@ -1,9 +1,99 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
-const Register = () => {
-    return(
-        <h1>Register Component</h1>
-    )
+import './Register.css';
+class Register extends Component {
+    state = {
+        registerForm:{
+            name: '',
+            email: '',
+            password: '',
+            cPassword: '',
+            role: '',
+            birthDate: '',
+            address: '',
+            companyName:'',
+            companyAddress: ''
+        },
+        token: ''
+    }
+    inputChangeHandler = (e, inputName) => {
+        let updatedRegisterForm = {
+            ...this.state.registerForm
+        }
+        updatedRegisterForm[inputName] = e.target.value;
+        this.setState({registerForm: updatedRegisterForm});
+    }
+    onRegister = (e) => {
+        e.preventDefault();
+        const payload = {
+            name: this.state.registerForm.name,
+            email: this.state.registerForm.email,
+            password: this.state.registerForm.password,
+            role: this.state.registerForm.role,
+            birthDate: this.state.registerForm.birthDate,
+            address: this.state.registerForm.address,
+            companyName: this.state.registerForm.companyName,
+            companyAddress: this.state.registerForm.companyAddress
+        }
+        console.log(payload);
+        axios.post('/user/register',payload).then(response=>{
+            console.log(response);
+        });
+    }
+    render(){
+        return(
+            <div className="row">
+            <div className="register-form">
+                <h1 className="page-header">Register</h1>        
+                    <div className="col-md-6">
+                    <form action="">
+                        <div className="form-group">
+                            <label htmlFor="name">Name</label>
+                            <input type="text" onChange={(event) => this.inputChangeHandler(event,'name')} className="form-control"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" onChange={(event) => this.inputChangeHandler(event,'email')} className="form-control"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" onChange={(event) => this.inputChangeHandler(event,'password')} className="form-control"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="cPassword">Confirm Password</label>
+                            <input type="password" onChange={(event) => this.inputChangeHandler(event,'cPassword')} className="form-control"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="role">What are you looking for?</label>
+                            <select className="form-control" onChange={(event) => this.inputChangeHandler(event,'role')}>
+                                <option value="employer">I am looking for candidates</option>
+                                <option value="candidate">I am looking for a job</option>
+                            </select>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="birthDate">Date of birth</label>
+                            <input type="date" onChange={(event) => this.inputChangeHandler(event,'birthDate')} className="form-control"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="address">Address</label>
+                            <input type="text" onChange={(event) => this.inputChangeHandler(event,'address')} className="form-control"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="companyName">Company Name (optional)</label>
+                            <input type="text" onChange={(event) => this.inputChangeHandler(event,'companyName')} className="form-control"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="companyAddress">Company Address</label>
+                            <input type="text" onChange={(event) => this.inputChangeHandler(event,'companyAddress')} className="form-control"/>
+                        </div>
+                        <button onClick={(event) => this.onRegister(event)} className="btn btn-primary" type="submit">Login</button>
+                    </form>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Register;
